4338a5fe7adfbbb301721747b06a23596fc21d94
WIL LAWRENZ .VALLO <wilvallo.edumail15@gmail.com>
2025-05-15 11:27:21 +0800

    test

---
+++ IT123P - Course Management Systemm/Login_Portal.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IT123P___Course_Management_Systemm
+{
+    public partial class Login_Portal : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

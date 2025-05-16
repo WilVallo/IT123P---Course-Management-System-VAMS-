cf2e17d3077bc75a16299d60ec3096ed9e919988
Wil Vallo <wilvallo.edumail15@gmail.com>
2025-05-16 16:02:40 +0800

    aa

---
+++ IT123P - Course Management Systemm/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IT123P___Course_Management_Systemm
+{
+    public class User
+    {
+        public string ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void AccessPortal();
+        public abstract void DisplayInfo();
+    }
+}
